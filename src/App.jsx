27fba48fb6dd86eec4eaa41a65603d787c5129bd
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-02-02 23:58:39 -0300

    Criando e estilizando o componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import { createGlobalStyle } from 'styled-components';
 import Header from './components/header';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
     border: 0;
     outline: none;
+    font-family: "Montserrat", serif;
   }
 
 `;
 
 const App = () => {
   return ( 
-    <Header />
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
  
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,71 @@
 import styled from "styled-components";
 
+// Parte "css"
 const HeaderContainer = styled.div` 
     display: flex;
-    justify-content: center;
-    border: 1px solid #000;
+    align-items: center;
+    /* border: 1px solid #000; */
+    padding: 8px;
 `;
 
-const StyledHeader = styled.div`
-    display: flex;
-    justify-content: center;
+const StyledLogo = styled.div`
+    margin-left: 50px;
+    /* display: flex; */
+    /* width: 50px; */
+    /* border: 1px solid #ff0000; */
    
 `;
 
-const Title = styled.p`
-    background-color: #8a2be2;
-    color: white;
+const StyledNavList =  styled.div`
+    margin-left: 300px;
+    ul {
+        display: flex;
+        align-items: center;
+        list-style: none;
+        font-weight: 500;
+        gap: 20px;
+        cursor: pointer;
+
+       & button {
+            height: 30px;
+            width: 60px;
+            border-radius: 5px;
+            border: 1px solid black;
+            color: #ffffff;
+            background-color: #000000;
+            cursor: pointer;
+
+        }
+
+        & button:hover {
+            background-color: #575151;
+            transition-duration: 0.3s;
+        }
+        & li:hover {
+            background-color: #e9e915ce;
+        }
+    }
+
+   
 `;
 
+// Parte "HTML"
 
 const Header = () => {
     return ( 
         <HeaderContainer>
-            <StyledHeader>
-                <Title>
-                    Hello, world!
-                </Title>
-            </StyledHeader>
+            <StyledLogo/>
+                <h3>FASHION</h3>
+            <StyledLogo/>
+            <StyledNavList>
+                <ul>
+                    <li>CATALOGUE</li>
+                    <li>FASHION</li>
+                    <li>FAVOURITE</li>
+                    <li>LIFESTYLE</li>
+                    <button>SING UP</button>
+                </ul>
+            </StyledNavList>
         </HeaderContainer>
         
      );
